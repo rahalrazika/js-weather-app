45accea75689bde7c4ed2abc7d80c47bd1140ea6
rahalrazika <rahalraika@gmail.com>
2021-02-01 19:20:33 +0100

    add switch button between Celsius and farenheit

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import 'tailwindcss/tailwind.css';
 import weatherContainer from './modules/weatherContainer';
 import fetchWeather from './modules/weatherfetch';
 
+
 document.getElementById('container').innerHTML = weatherContainer();
 fetchWeather('algeria');
 weatherContainer();
@@ -9,7 +10,9 @@ weatherContainer();
 const search = () => {
   fetchWeather(document.querySelector('.search-bar').value);
 };
+
 document.querySelector('.search button').addEventListener('click', () => {
+  fetchWeather();
   search();
 });
 
@@ -17,6 +20,7 @@ document
   .querySelector('.search-bar')
   .addEventListener('keyup', (event) => {
     if (event.key === 'Enter') {
+      fetchWeather();
       search();
     }
   });
--- src/modules/displayForm.js
+++ src/modules/displayForm.js
@@ -1,4 +1,4 @@
-import unite from './unite';
+import switchButton from './convertTemp';
 
 const displayWeather = (data) => {
   const { name } = data;
@@ -8,8 +8,8 @@ const displayWeather = (data) => {
   document.querySelector('.city').innerText = `Weather in ${name}`;
   document.querySelector('.icon').src = `https://openweathermap.org/img/wn/${icon}.png`;
   document.querySelector('.description').innerText = description;
-  // document.querySelector('.temp').innerText = ` ${Math.round(temp - 273)} °C`;
-  document.querySelector('.temp').addEventListener('click', () => { unite(temp); });
+  document.querySelector('.temp').innerText = `${Math.round(temp)} °C`;
+  document.querySelector('.temp').addEventListener('click', () => switchButton(temp));
   document.querySelector('.humidity').innerText = `Humidity: ${humidity}%`;
   document.querySelector('.wind').innerText = `Wind speed: ${speed} km/h`;
   document.body.style.backgroundImage = `url('https://source.unsplash.com/1600x900/?${name},${description})`;
